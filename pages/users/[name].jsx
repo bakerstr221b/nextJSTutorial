2c860877fb316a35c7b09d0993008fe64310bd2e
bakerstr221b <holmes81@gmail.com>
2023-02-19 09:49:31 +0900

    예외처리 추가, 버그 수정

--- pages/users/[name].jsx
+++ pages/users/[name].jsx
@@ -1,23 +1,28 @@
 import fetch from "isomorphic-unfetch";
 
 const name = ({ user }) => {
+  console.log("user    :  " + JSON.stringify(user));
   const username = user && user.name;
   return <div>{username}</div>;
 };
 
 export const getServerSideProps = async ({ query }) => {
-  console.log("query   :  " + query);
+  console.log("query   :  " + JSON.stringify(query));
   const { name } = query;
   try {
     const res = await fetch(`http://api.github.com/users/${name}`);
+    console.log("res.status  :  " + res.status);
     if (res.status === 200) {
       const user = await res.json();
       console.log(" success"); //터미널에서 출력
       return { props: { user } };
+    } else if (res.status === 404) {
+      return { props: { user: { name: "없음" } } };
+    } else {
+      return { props: {} };
     }
-    return { pros: {} };
   } catch (e) {
-    console.log(e);
+    console.log("ee   :   " + e);
     return { props: {} };
   }
 };
