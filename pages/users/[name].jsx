bb2e689642aed0fa5dc4c3061223c3951bb6fa9a
bakerstr221b <holmes81@gmail.com>
2023-02-20 01:38:08 +0900

    2.3.3 getInitialProps

--- pages/users/[name].jsx
+++ pages/users/[name].jsx
@@ -6,24 +6,19 @@ const name = ({ user }) => {
   return <div>{username}</div>;
 };
 
-export const getServerSideProps = async ({ query }) => {
-  console.log("query   :  " + JSON.stringify(query));
+name.getInitialProps = async ({ query }) => {
   const { name } = query;
   try {
     const res = await fetch(`http://api.github.com/users/${name}`);
     console.log("res.status  :  " + res.status);
     if (res.status === 200) {
       const user = await res.json();
-      console.log(" success"); //터미널에서 출력
-      return { props: { user } };
-    } else if (res.status === 404) {
-      return { props: { user: { name: "없음" } } };
-    } else {
-      return { props: {} };
+      return { user };
     }
+    return { props: {} };
   } catch (e) {
     console.log("ee   :   " + e);
-    return { props: {} };
+    return {};
   }
 };
 
